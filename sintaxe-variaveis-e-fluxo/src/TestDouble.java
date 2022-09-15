f26480bfa3026ff94de8b926f2edebc21e820422
Silvanis <silvanis.silva@outlook.com>
2022-09-14 23:13:33 -0300

    Enviando estudos de hoje.

---
+++ sintaxe-variaveis-e-fluxo/src/TestDouble.java
@@ -0,0 +1,14 @@
+
+public class TestDouble {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.40;
+		System.out.println(salario);
+		
+		double calculando = 5.0 / 2;
+		System.out.println(calculando);
+		
+	}
+	
+}
