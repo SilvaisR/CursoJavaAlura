a0afea14726b57193d190948e273c8df703ddc2c
Silvanis <silvanis.silva@outlook.com>
2022-09-15 22:05:33 -0300

    ainda continuando em ifs e aprendendo escopo

--- sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -2,10 +2,11 @@
 public class TestaCondicional2 {
 	public static void main(String[] args) {
 		
-		int idade = 16;
-		int quantidadePessoas = 3;
+		int idade = 18;
+		int quantidadePessoas = 1;
+		boolean acompanhado = quantidadePessoas >=2;
 		
-		if(idade >= 18 || quantidadePessoas >= 2) {
+		if(idade >= 18 && acompanhado) {
 			System.out.println("Seja muito bem vindo!");
 		} else {
 			System.out.println("Você não pode entrar!");
---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,17 @@
+
+public class TestaEscopo {
+	public static void main(String[] args) {
+		
+		int quantidadePessoas = 1;
+		boolean acompanhado;
+		
+		if(quantidadePessoas >= 2) {
+			  acompanhado = true;
+		}else {
+			  acompanhado = false;
+		}
+		
+		System.out.println(acompanhado);
+		
+	}
+}
