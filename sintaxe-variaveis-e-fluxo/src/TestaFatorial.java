d6da4b82a5b3c613b5eee18f0da01d1a00acfa09
Silvanis <silvanis.silva@outlook.com>
2022-09-19 11:43:03 -0300

    Adicionando conteúdos das aulas, terminei o curso básico do Java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaFatorial.java
@@ -0,0 +1,16 @@
+
+public class TestaFatorial {
+	
+	public static void main(String[] args) {
+		
+		int fatorial = 1;
+		for(int contador = 1; contador < 11; contador++) {
+			
+			fatorial *= contador;
+			System.out.println(contador);
+			
+		}
+		
+	}
+
+}
